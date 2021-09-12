1482b62f99085f23c4a7cb236f9ad9236631beed
Gabomfim <gabomfim99@gmail.com>
2021-09-12 15:34:53 -0300

    pathCost

--- infect.hs
+++ infect.hs
@@ -13,7 +13,8 @@ finalResult nodes = maximum (map (\nodes -> snd nodes) nodes)
 -- Poda a árvore de busca, selecionando apenas os caminhos que valem a pena.
 pruneTree :: ([Char], [Char]) -> [(([Char], [Char]), Float)] -> [([Char], Float)] -> [[Char]] -> [[Char]]
 pruneTree (origin, destiny) relations nodes queue
-    | (getNodeCost destiny nodes) > ((getNodeCost origin nodes) + (getRelationWeight (origin, destiny) relations)) = queue++[destiny] -- Se o candidato valer a pena, adicionar na fila
+    | (getNodeCost destiny nodes) > pathCost (origin, destiny) relations nodes = queue++[destiny] -- Se o candidato valer a pena, adicionar na fila
     | otherwise = queue
     
-
+pathCost :: ([Char], [Char]) -> [(([Char], [Char]), Float)] -> [([Char], Float)] -> Float
+pathCost (origin, destiny) relations nodes = ((getNodeCost origin nodes) + (getRelationWeight (origin, destiny) relations))
