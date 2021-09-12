db1014b9ce961454e7ac3675c72aea4be6b356ee
Gabomfim <gabomfim99@gmail.com>
2021-09-12 15:01:20 -0300

    type fix

--- infect.hs
+++ infect.hs
@@ -1,6 +1,14 @@
+getNodeCost :: [Char] -> [([Char], Float)] -> Float
+getNodeCost name nodes = snd (head (filter (\node ->  fst node == name) nodes))
 
-getAccWeight name nodes = snd (filter (\node ->  fst node == name) nodes)
+getRelationWeight :: ([Char], [Char]) -> [(([Char], [Char]), Float)] -> Float
+getRelationWeight (origin, destiny) relations = snd (head (filter (\relation ->  fst relation == (origin, destiny)) relations))
 
-finalResult :: [([Char], Int)] -> Int
-
+finalResult :: [([Char], Float)] -> Float
 finalResult nodes = maximum (map (\nodes -> snd nodes) nodes)
+
+
+
+-- Poda a árvore de busca, selecionando apenas os caminhos que valem a pena
+-- pruneTree origin destiny queue = soma o peso da relacao de origem pra destino, se o candidato valer a pena, adicionar na fila
+
