93a15e88cd3e45ada603beb9ddda77c9647e6a2a
Gabomfim <gabomfim99@gmail.com>
2021-09-12 22:35:30 -0300

    organization

--- infect.hs
+++ infect.hs
@@ -1,3 +1,6 @@
+
+-- AUX METHODS --
+
 setNodeCost :: [Char] -> [([Char], Float)] -> Float -> [([Char], Float)]
 setNodeCost name nodes value = map (\node -> if fst node == name then (name, value) else node) nodes
 
@@ -17,6 +20,41 @@ createOriginDestinyList relations nodes queue = map (\destiny -> ((head queue), 
 -- get all the destinies that has origin "origin"
 getDestinies :: [Char] -> [(([Char], [Char]), Float)] -> [[Char]]
 getDestinies origin relations = map (\((origin, destiny), weight) -> destiny) (filter (\relation ->  fst (fst relation) == origin) relations)
+
+
+-- INPUT --
+
+-- cria a tupla para representar o grafo e as distâncias dos nós
+interpreter :: [String] -> ([(String, Float)], [((String, String), Float)])
+interpreter lista = (patientZero lista [], createRelations lista)
+
+-- cria um vetor de tuplas (nome, dist) que representam os nos e suas distancias
+patientZero :: [String] -> [(String, Float)] -> [(String, Float)]
+-- entrada chegou no final, coloca o nó inicial com distância 0, coloca o infectado numa fila e retorna
+patientZero [nome] distancias = setNodeCost nome distancias 0
+patientZero (nome1:nome2:dist:rest) distancias =  patientZero rest dist
+    where dist = distancias ++ (createTuple nome1 distancias) ++ (createTuple nome2 distancias)
+
+--função auxiliar para checar se nome existe em tupla de nos
+checkName :: String -> [(String, Float)] -> Bool
+checkName name [] = False
+checkName name (a:as)
+    | name == fst a = True
+    | otherwise = checkName name as
+
+-- cria tupla se nome não estiver presente no vetor original
+createTuple :: String -> [(String, Float)] -> [(String, Float)]
+createTuple name list 
+    | checkName name list = []
+    | otherwise = [(name, 1/0)]
+
+-- cria um vetor de ((nome, nome), dist) que representa as arestas do grafo
+createRelations :: [String] ->  [((String, String), Float)]
+createRelations [nome] = []
+createRelations (nome1:nome2:dist:rest) = [((nome1, nome2), (read dist :: Float)), ((nome2, nome1), (read dist :: Float))] ++ createRelations rest
+
+
+-- ALGORITMO --
 
 finalResult :: [([Char], Float)] -> Float
 finalResult nodes = maximum (map (\nodes -> snd nodes) nodes)
@@ -42,18 +80,18 @@ updateCostIfWorth (origin, destiny) relations nodes
     | otherwise = nodes
 
 -- Executa updateCostIfWorth e pruneTree. Devolve uma tupla com os resultados.
-step :: ([Char], [Char]) -> [(([Char], [Char]), Float)] -> [([Char], Float)] -> ([([Char], Float)], [[Char]])
-step (origin, destiny) relations nodes = (updateCostIfWorth (origin, destiny) relations nodes, pruneTree (origin, destiny) relations nodes (getDestinies origin relations))
+infect :: ([Char], [Char]) -> [(([Char], [Char]), Float)] -> [([Char], Float)] -> ([([Char], Float)], [[Char]])
+infect (origin, destiny) relations nodes = (updateCostIfWorth (origin, destiny) relations nodes, pruneTree (origin, destiny) relations nodes (getDestinies origin relations))
 
 
 
 singleLevelDFS :: [(([Char], [Char]), Float)] -> ([([Char], Float)], [[Char]]) -> [([Char], [Char])] -> ([([Char], Float)], [[Char]])
 singleLevelDFS _ (nodes, queue) [] = (nodes, queue)
-singleLevelDFS relations (nodes, nextInQueue) originDestinyList = singleLevelDFS relations (step (head originDestinyList) relations nodes) (tail originDestinyList)
+singleLevelDFS relations (nodes, nextInQueue) originDestinyList = singleLevelDFS relations (infect (head originDestinyList) relations nodes) (tail originDestinyList)
 
 -- gere uma lista (origem, destino) para o primeiro item da fila de nomes
 -- escolha a primeira tupla (origem, destino) da lista
--- execute step essa tupla (origem, destino) -> isso removerá a origem da fila de nomes, colocará os destinos nessa fila e atualizará o que for necessário
+-- execute infect essa tupla (origem, destino) -> isso removerá a origem da fila de nomes, colocará os destinos nessa fila e atualizará o que for necessário
 -- remova essa tupla origem destino da lista de origens e destinos
 -- quando essa lista ficar vazia, execute o algoritmo novamente
 wrapper :: [(([Char], [Char]), Float)] -> ([([Char], Float)], [[Char]]) -> [([Char], [Char])] -> ([([Char], Float)], [[Char]])
@@ -66,42 +104,3 @@ run relations (nodes, queue) = finalResult (fst (wrapper relations (nodes, queue
 
 -- TESTCASE:
 -- run [(("a", "b"), 2), (("b", "a"), 2), (("a", "c"), 3), (("c", "a"), 3), (("c", "d"), 4), (("d", "c"), 4)] ([("a", 0) , ("b", 1/0), ("c", 1/0), ("d", 1/0)], ["a"])
-
-
---- FE que colocou isso aqui ----
--- cria a tupla para representar o grafo e as distâncias dos nós
-interpreter :: [String] -> ([(String, Float)], [((String, String), Float)])
-interpreter lista = (createDistances lista [], createRelations lista)
-
--- cria um vetor de tuplas (nome, dist) que representam os nos e suas distancias
-createDistances :: [String] -> [(String, Float)] -> [(String, Float)]
--- entrada chegou no final, coloca o nó inicial com distância 0
-createDistances [nome] distancias = setNodeCost nome distancias 0
-createDistances (nome1:nome2:dist:rest) distancias =  createDistances rest dist
-    where dist = distancias ++ (createTuple nome1 distancias) ++ (createTuple nome2 distancias)
-
---função auxiliar para checar se nome existe em tupla de nos
-checkName :: String -> [(String, Float)] -> Bool
-checkName name [] = False
-checkName name (a:as)
-    | name == fst a = True
-    | otherwise = checkName name as
-
--- cria tupla se nome não estiver presente no vetor original
-createTuple :: String -> [(String, Float)] -> [(String, Float)]
-createTuple name list 
-    | checkName name list = []
-    | otherwise = [(name, 1/0)]
-
--- cria um vetor de ((nome, nome), dist) que representa as arestas do grafo
-createRelations :: [String] ->  [((String, String), Float)]
-createRelations [nome] = []
-createRelations (nome1:nome2:dist:rest) = [((nome1, nome2), (read dist :: Float)), ((nome2, nome1), (read dist :: Float))] ++ createRelations rest
-
-
-
-
-
--- escolhe o primeiro da fila e executa getDestinies, depois execute step com todos a origem e destinos obtidos
--- wrapper _ _ nodes [] = (nodes, [])
---wrapper relations nodes queue = foldl (\relation -> step relation relations nodes queue) (nodes, queue) 
